b6156b54a11eb3be15198526095f940a455ad6eb
Alexander <al.mironov88@gmail.com>
2024-04-05 15:43:27 +0200

    new commit

--- models/chicago_taxi_trips/last_week_trips.sql
+++ models/chicago_taxi_trips/last_week_trips.sql
@@ -3,5 +3,5 @@ from {{ source('chicago_taxi_trips', 'taxi_trips')}}
 where trip_start_timestamp >= timestamp_sub(
     (select max(trip_start_timestamp)
     from {{ source('chicago_taxi_trips', 'taxi_trips')}}),
-    interval 7 day
-)
+    interval 7 day) 
+    and trip_start_timestamp >= trip_end_timestamp
